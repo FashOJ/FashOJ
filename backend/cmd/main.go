c36d3f04706489cc2f677a0c415e26c5313bc612
Lingbou <2180717380@qq.com>
2025-03-11 11:11:18 +0800

    modified:router,main

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,22 +1,10 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
+	"FashOJ/backend/routes"
 )
 
-// 测试程序
 func main() {
-	r := gin.Default()
-
-	// 测试路由
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "pong"})
-	})
-
-	// 运行服务器
-	if err := r.Run(":8080"); err != nil {
-		log.Fatal("Failed to start server: ", err)
-	}
+	r := routes.SetupRouter()
+	r.Run(":8080")
 }
