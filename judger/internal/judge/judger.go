0a164cde44797290d025d8b63e371ec74e8dd630
Lingbou <2180717380@qq.com>
2025-03-28 00:20:31 +0800

    TEST VERSION: add seccomp and get it to judger

--- judger/internal/judge/judger.go
+++ judger/internal/judge/judger.go
@@ -5,18 +5,22 @@ import (
 	"os"
 )
 
-func Judge(sourcePath, execPath, inputPath, answerPath string, timeLimit int) string {
+func Judge(sourcePath, execPath, inputPath, answerPath string, timeLimit int, memoryLimit int64) string {
 	if err := CompileCpp17(sourcePath, execPath); err != nil {
 		return fmt.Sprintf("编译错误: %v", err)
 	}
-	output, err := Run(execPath, inputPath, timeLimit)
+	fmt.Println("编译好了")
+	output, err := Run(execPath, inputPath, timeLimit, memoryLimit)
 	if err != nil {
 		return fmt.Sprintf("运行错误: %v", err)
 	}
+	fmt.Println("运行好了")
 	match, err := Compare(output, answerPath)
 	if err != nil {
 		return fmt.Sprintf("答案错误: %v", err)
 	}
+
+	fmt.Println("评测好了")
 
 	defer os.Remove(execPath)
 
