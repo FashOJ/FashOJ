32912b68c85c893b62640838558b982b5ce0c9eb
zine yu <zine.xlws@gmail.com>
2025-05-04 13:26:07 +0800

    feat(@/components/navbar.vue): 添加NavBar

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#">Fash OJ</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Problems</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contest</a>
+          </li>
+        </ul>
+        <span class="navbar-text">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              zine yu
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+</script>
+
+<style scoped>
+
+.navbar {
+  background-color: #fff !important;
+}
+
+.container {
+  width: 80vw;
+}
+
+.dropdown {
+  list-style: none;
+}
+</style>
