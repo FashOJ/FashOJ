1a9b5cac314891d91c7858f23ac6e9d2648de541
Lingbou <2180717380@qq.com>
2025-05-11 18:37:40 +0800

    pref(judger/cmd/main.go):auto path

--- judger/cmd/main.go
+++ judger/cmd/main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"FashOJ/Judger/internal/judge"
 	"fmt"
+	"path/filepath"
+	"runtime"
 )
 
 func main() {
-	sourceCPP := "../testTEMP/sourceCode/testCPP.cpp"
-	// exec := "../testTEMP/execTemp/test"
-	// exec := "/home/lingbou/WorkSpace/FashOJ/judger/testTEMP/execTemp/test" // shit ！！！，记得后面把相对路径全换成绝对路径
-	exec := "/home/lingbou/FashOJ/judger/testTEMP/execTemp/test" 
-	///home/lingbou/WorkSpace/FashOJ/judger/testTEMP
-	input := "../testTEMP/testCase/input1.txt"
-	answer := "../testTEMP/outputCase/output1.txt"
-	timeLimit := 200000 // 1s
+	_, filename, _, _ := runtime.Caller(0)
+	baseDir := filepath.Dir(filepath.Dir(filename))
+	sourceCPP := filepath.Join(baseDir, "testTEMP", "sourceCode", "testCPP.cpp")
+	exec := filepath.Join(baseDir, "testTEMP", "execTemp", "test")
+	input := filepath.Join(baseDir, "testTEMP", "testCase", "input1.txt")
+	answer := filepath.Join(baseDir, "testTEMP", "outputCase", "output1.txt")
+	timeLimit := 1000 // 1s
 
 	result := judge.Judge(sourceCPP, exec, input, answer, timeLimit)
 	fmt.Println("评测结果:", result)
