aecdd4667b2a43493e78080099c3b7eedf7d9abd
zine yu <zine.xlws@gmail.com>
2025-05-13 19:02:20 +0800

    feat(controllers/announcement): impl GET /api/announcement

--- backend/dto/announcement.go
+++ backend/dto/announcement.go
@@ -6,6 +6,21 @@ type LastAnnouncement struct {
 }
 
 type CreateAnnouncement struct {
-	Title string `json:"title"`
+	Title   string `json:"title"`
 	Content string `json:"content"`
+}
+
+type Announcement struct {
+	Id       int    `json:"id"`
+	Author   string `json:"author"`
+	Avatar   string `json:"avatar"`
+	AuthorId int    `json:"author_id"`
+	Title    string `json:"title"`
+	Abstract string `json:"abstract"`
+}
+
+type AnnouncementPage struct {
+	Pages         int            `json:"pages"`
+	Size          int            `json:"size"`
+	Announcements []Announcement `json:"announcements"`
 }
