b8b6a81fc8a5e196f50366650eb075d107b1c984
Lingbou <2180717380@qq.com>
2025-08-07 20:16:38 +0800

    Clear all files from main branch

--- backend/internal/router/normalSubmit.go
+++
@@ -1,9 +0,0 @@
-package router
-
-import "github.com/gin-gonic/gin"
-
-func setupNormalSubmitRoutes(r *gin.RouterGroup) {
-	
-	normalSubmit := r.Group("/submit")
-	normalSubmit.POST("")
-}
--- judger/internal/dto/createProblem.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-type CreateProblem struct {
-	ProblemId uint `json:"problem_id"`
-	TimeLimit uint `json:"time_limit"`
-	MemoryLimit uint `json:"memory_limit"`
-}
